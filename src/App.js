c3c536ca70969b885d9367768fb135df7ddab353
Neelam <neelam9673147@gmail.com>
2020-12-26 23:31:43 +0530

    app completed

--- src/App.js
+++ src/App.js
@@ -1,25 +1,59 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
+import axios from "axios";
+import Loader from "./components/loader";
+import { Card, Button } from "react-bootstrap";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    advice: "",
+    count: Math.floor(Math.random() * 10 + 1),
+    id: 0,
+    isLoading: false,
+  };
+
+  componentDidMount() {
+    console.log("mount");
+    this.fetchAdvice();
+  }
+
+  fetchAdvice = () => {
+    this.setState({ count: this.state.count + 1, isLoading: true });
+
+    axios
+      .get(`https://api.adviceslip.com/advice/${this.state.count}`)
+      .then((response) => {
+        let ans = response.data + "}";
+        let data = JSON.parse(ans);
+        console.log(data);
+        this.setState({
+          advice: data.slip.advice,
+          isLoading: false,
+        });
+      })
+
+      .catch((error) => {
+        console.log(error);
+        this.setState({ isLoading: false });
+      });
+  };
+
+  render() {
+    const { isLoading, advice } = this.state;
+    console.log("render");
+    console.log(advice);
+
+    return (
+      <div className="App">
+        <Card className="card1">
+          {isLoading ? <Loader /> : <h2 className="heading">{advice}</h2>}
+          <Button className="advice-btn" onClick={this.fetchAdvice}>
+            Give Me Advice !
+          </Button>
+        </Card>
+      </div>
+    );
+  }
 }
 
 export default App;
